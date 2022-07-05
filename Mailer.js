2b49328af4f9ed02dd86d29853a47ecf3ef0d7bc
Tyler Riffle <riffletyler3@gmail.com>
2022-07-04 18:43:13 -0600

    Added ability to send email from gmail account

--- Mailer.js
+++ Mailer.js
@@ -1,3 +1,23 @@
+import nodemailer from "nodemailer"
+
 class Mailer {
-    constructor() {}
+    constructor(email, pass) {
+        this.email = email
+
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: { user: this.email, pass }
+        })
+    }
+
+    sendEmail(to, subject, text) {
+        this.transporter.sendMail({
+            email: this.email,
+            to,
+            subject,
+            text
+        })
+    }
 }
+
+export default Mailer
